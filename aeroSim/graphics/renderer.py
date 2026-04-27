f1bdaed381d040644b2a066aabe185841a7dcdc2
Zuchi <zuchii.victor@gmail.com>
2026-04-27 12:19:02 -0300

    Ajustei para melhor visualização

--- aeroSim/graphics/renderer.py
+++ aeroSim/graphics/renderer.py
@@ -10,6 +10,7 @@ class Renderer:
         pygame.display.set_caption("Aero Simulator")
         self.gridRes = gridRes
         self.font = pygame.font.Font(None, 36)
+        self.offset_y = -200  # Offset para mover elementos para cima
 
     def render(self, world):
         self.screen.fill((30, 30, 30))
@@ -18,26 +19,26 @@ class Renderer:
         for obs in world.obstacles.getAll():
             if isinstance(obs, Circle):
                 # Desenhar círculo
-                pygame.draw.circle(self.screen, (150, 150, 150), (int(obs.cx), int(obs.cy)), int(obs.radius), 2)
-                pygame.draw.circle(self.screen, (100, 100, 100), (int(obs.cx), int(obs.cy)), int(obs.radius) - 2)
+                pygame.draw.circle(self.screen, (150, 150, 150), (int(obs.cx), int(obs.cy + self.offset_y)), int(obs.radius), 2)
+                pygame.draw.circle(self.screen, (100, 100, 100), (int(obs.cx), int(obs.cy + self.offset_y)), int(obs.radius) - 2)
             elif isinstance(obs, Polygon):
                 # Desenhar polígono (retângulo)
                 corners = obs.getCorners()
-                points = [(int(x), int(y)) for x, y in corners]
+                points = [(int(x), int(y + self.offset_y)) for x, y in corners]
                 pygame.draw.polygon(self.screen, (100, 100, 150), points)
                 pygame.draw.polygon(self.screen, (150, 150, 200), points, 2)
             elif isinstance(obs, Wedge):
                 # Desenhar funil em V
                 corners = obs.getCorners()
-                points = [(int(x), int(y)) for x, y in corners]
+                points = [(int(x), int(y + self.offset_y)) for x, y in corners]
                 pygame.draw.polygon(self.screen, (120, 100, 150), points)
                 pygame.draw.polygon(self.screen, (180, 150, 220), points, 3)
             elif isinstance(obs, Roof):
                 # Desenhar telhado (linha inclinada)
                 corners = obs.getCorners()
                 if len(corners) == 2:
-                    p1 = (int(corners[0][0]), int(corners[0][1]))
-                    p2 = (int(corners[1][0]), int(corners[1][1]))
+                    p1 = (int(corners[0][0]), int(corners[0][1] + self.offset_y))
+                    p2 = (int(corners[1][0]), int(corners[1][1] + self.offset_y))
                     pygame.draw.line(self.screen, (200, 200, 200), p1, p2, 4)
         
         # SANDBOX: Desenhar múltiplas partículas
@@ -51,7 +52,7 @@ class Renderer:
                 pygame.draw.circle(
                     self.screen, 
                     color, 
-                    (int(particle.x), int(particle.y)), 
+                    (int(particle.x), int(particle.y + self.offset_y)), 
                     int(particle.radius)
                 )
         
@@ -60,7 +61,7 @@ class Renderer:
             pygame.draw.circle(
                 self.screen, 
                 (255, 50, 50), 
-                (int(world.particle.x), int(world.particle.y)), 
+                (int(world.particle.x), int(world.particle.y + self.offset_y)), 
                 8
             )
         
